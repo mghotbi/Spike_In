5e9a0b79df0408501052b6cc2c6b48786d491c94
Mitra <mitra.ghotbi@gmail.com>
2024-03-19 13:37:24 -0500

    Update SPIKEIN_Tetragenococcus_halophilus.R

--- SPIKEIN_Tetragenococcus_halophilus.R
+++ SPIKEIN_Tetragenococcus_halophilus.R
@@ -5,13 +5,19 @@
 # --------------------------------------------------------
 
 # Load necessary libraries
-suppressMessages(library(phyloseq, dplyr, microbiome,ape))
+suppressMessages({
+  library(ape)
+  library(phyloseq)
+  library(dplyr)
+  library(microbiome)
+})
 
 # working directory
 setwd("C:/Users/mghotbi/Desktop/R results16s_21/collection/UHM/Cl_Al_16S_Feb24-20240217T150211Z-001/Cl_Al_16S_Feb24")
 
 # Load and tidy
-physeq16S <- readRDS("physeq16S.rds")
+physeq16S <- readRDS("physeq_pure_16S.rds")
+
 set.seed(123)
 physeq16S <- tidy_phyloseq(physeq16S)
 
@@ -19,18 +25,17 @@ physeq16S <- tidy_phyloseq(physeq16S)
 physeq16S_NO_Tetragenococcus_halophilus <- physeq16S %>%
   subset_taxa(Species != "Tetragenococcus_halophilus")
 saveRDS(physeq16S_NO_Tetragenococcus_halophilus, file = "physeq16S_NO_Tetragenococcus_halophilus.rds")
-meta <- sample_data(physeq16S)
+
 
 # Define spiked cell counts
 spiked_cells_s_Dekkera_bruxellensis <- 733
 spiked_cells_s_Tetragenococcus_halophilus <- 1847
 
-# Exclude irrelevant samples and spike-in normalization
-spiked_16S <- subset_samples(physeq16S, !(sample.id %in% c(
-  "UHM746-21478_S117", "UHM747-21477_S106", "UHM748-21467_S170", "UHM748-21487_S129", 
-  "UHM749-21479_S128", "UHM759-21466_S159", "UHM759-21486_S118", "UHM775-21485_S107", "UHM776-21482_S161", "UHM777-21484_S183", "UHM779-21468_S181", "UHM779-21488_S140", "UHM782-21480_S139", "UHM810-21472_S138", "UHM811-21471_S127", "UHM813-21481_S150", "UHM818-21469_S105", "UHM818-21489_S151", "UHM819-21473_S149", "UHM820-21470_S116",
-  "UHM820-21490_S162", "UHM827-21474_S160", "UHM829-21476_S182", "UHM832-21483_S172", "UHM905-20598_S45"
-)))
+
+#physeq16S@sam_data$plate.ID
+
+# Exclude irrelevant samples and keep those spiked 
+spiked_16S <- subset_samples(physeq16S, plate.ID %in% c("BAD_plate", "AlexRurik_MAD_plate1", "22UHMwf_JDp3_MAD2", "22UHMwf_JDp1"))
 
 #Normalize based on the spiked volume of the samples
 if (class(spiked_16S@otu_table) == "matrix") {
@@ -40,7 +45,7 @@ if (class(spiked_16S@otu_table) == "matrix") {
 }
 physeq16S_adj_scaled <- spiked_16S
 
-# scaling factor
+# scaling factor calculation starts from here
 spiked_16S_total_reads <- data.frame(Sample = rownames(sample_data(physeq16S_adj_scaled)), Total_Reads = sample_sums(otu_table(physeq16S_adj_scaled)))
 write.csv(spiked_16S_total_reads, file = "spiked_16S_total_reads.csv")
 
@@ -51,31 +56,29 @@ denominator_spiked_Tetragenococcus_halophilus_reads <- spiked_Tetragenococcus_ha
 scaling_factor_Tetragenococcus_halophilus <- ifelse(denominator_spiked_Tetragenococcus_halophilus_reads != 0, spiked_cells_s_Tetragenococcus_halophilus / denominator_spiked_Tetragenococcus_halophilus_reads, 0)
 write.csv(scaling_factor_Tetragenococcus_halophilus, file = "scaling_factor_Tetragenococcus_halophilus.csv", row.names = FALSE)
 
-# Scale the ASV counts by the scaling factor and round the result
+# Scale the ASV counts by crossing ASVs to the scalling factor and round the result
 physeq16S_adj_scaled_count <- round(otu_table(physeq16S_adj_scaled) * scaling_factor_Tetragenococcus_halophilus)
 write.csv(physeq16S_adj_scaled_count, file = "physeq16S_adj_scaled_AbsoluteCount.csv")
 
-# Create a new phyloseq object with the calculated absolute counts
+# Create a new phyloseq object with the calculated absolute abundance
 otu <- read.csv("physeq16S_adj_scaled_AbsoluteCount.csv", header = TRUE, sep = ",", row.names = 1)
 otumat <- as.matrix(otu)
-random_tree = rtree(ntaxa(adj_spiked_16S), rooted=TRUE, tip.label=taxa_names(adj_spiked_16S))
+random_tree = rtree(ntaxa(physeq16S_adj_scaled), rooted=TRUE, tip.label=taxa_names(physeq16S_adj_scaled))
 
 adj_scaled_spiked_16S <- phyloseq(otu_table(otumat, taxa_are_rows = TRUE),
                            tax_table(physeq16S_adj_scaled),
                            phy_tree(random_tree),
                            sample_data(physeq16S_adj_scaled))    
 
-#Alternatively you could save time by directly crossing spiked-in calling factor to the ASV table while you are making you new phyloseq object
+#Alternatively you could save your time by directly crossing spiked-in calling factor to the ASV table of your new phyloseq object
 
-# Create new phyloseq object with scaled ASV x spiked-in calling factor 
-random_tree <- rtree(ntaxa(spiked_16S), rooted = TRUE, tip.label = taxa_names(spiked_16S))
-physeq16S_adj_scaled <- phyloseq(
-  otu_table(otu_table(spiked_16S) * scaling_factor_Tetragenococcus_halophilus, taxa_are_rows = TRUE),
+random_tree <- rtree(ntaxa(physeq16S_adj_scaled), rooted = TRUE, tip.label = taxa_names(physeq16S_adj_scaled))
+physeq16S_adj_scaled_absolute_abundance <- phyloseq(
+  otu_table(otu_table(physeq16S_adj_scaled) * scaling_factor_Tetragenococcus_halophilus, taxa_are_rows = TRUE),
   tax_table(spiked_16S),
   phy_tree(random_tree),
   sample_data(spiked_16S)
 )
 
 print("You did it, Congratulations!")
-
-                 
+saveRDS(physeq16S_adj_scaled_absolute_abundance,"physeq16S_adj_scaled_absolute_abundance240319.rds")            
