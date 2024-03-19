430f712220aad0edc7d878d9a83d4880d7eb1377
Mitra <mitra.ghotbi@gmail.com>
2024-03-18 22:38:20 -0500

    Update SPIKEIN_Tetragenococcus_halophilus.R

--- SPIKEIN_Tetragenococcus_halophilus.R
+++ SPIKEIN_Tetragenococcus_halophilus.R
@@ -5,7 +5,7 @@
 # --------------------------------------------------------
 
 # Load necessary libraries
-suppressMessages(library(phyloseq, dplyr, microbiome))
+suppressMessages(library(phyloseq, dplyr, microbiome,ape))
 
 # working directory
 setwd("C:/Users/mghotbi/Desktop/R results16s_21/collection/UHM/Cl_Al_16S_Feb24-20240217T150211Z-001/Cl_Al_16S_Feb24")
@@ -31,22 +31,16 @@ spiked_16S <- subset_samples(physeq16S, !(sample.id %in% c(
   "UHM749-21479_S128", "UHM759-21466_S159", "UHM759-21486_S118", "UHM775-21485_S107", "UHM776-21482_S161", "UHM777-21484_S183", "UHM779-21468_S181", "UHM779-21488_S140", "UHM782-21480_S139", "UHM810-21472_S138", "UHM811-21471_S127", "UHM813-21481_S150", "UHM818-21469_S105", "UHM818-21489_S151", "UHM819-21473_S149", "UHM820-21470_S116",
   "UHM820-21490_S162", "UHM827-21474_S160", "UHM829-21476_S182", "UHM832-21483_S172", "UHM905-20598_S45"
 )))
+
+#Normalize based on the spiked volume of the samples
 if (class(spiked_16S@otu_table) == "matrix") {
   spiked_16S@otu_table <- spiked_16S@otu_table / 3
 } else {
   spiked_16S@otu_table <- as.matrix(spiked_16S@otu_table) / 3
 }
+physeq16S_adj_scaled <- spiked_16S
 
-# Create new phyloseq object with scaled ASV counts
-random_tree <- rtree(ntaxa(spiked_16S), rooted = TRUE, tip.label = taxa_names(spiked_16S))
-physeq16S_adj_scaled <- phyloseq(
-  otu_table(otu_table(spiked_16S) * scaling_factor_Tetragenococcus_halophilus, taxa_are_rows = TRUE),
-  tax_table(spiked_16S),
-  phy_tree(random_tree),
-  sample_data(spiked_16S)
-)
-
-# Calculate scaling factors for Tetragenococcus_halophilus and save results
+# scaling factor
 spiked_16S_total_reads <- data.frame(Sample = rownames(sample_data(physeq16S_adj_scaled)), Total_Reads = sample_sums(otu_table(physeq16S_adj_scaled)))
 write.csv(spiked_16S_total_reads, file = "spiked_16S_total_reads.csv")
 
@@ -58,15 +52,30 @@ scaling_factor_Tetragenococcus_halophilus <- ifelse(denominator_spiked_Tetrageno
 write.csv(scaling_factor_Tetragenococcus_halophilus, file = "scaling_factor_Tetragenococcus_halophilus.csv", row.names = FALSE)
 
 # Scale the ASV counts by the scaling factor and round the result
-physeq16S_adj_scaled <- round(otu_table(physeq16S_adj_scaled) * scaling_factor_Tetragenococcus_halophilus)
-write.csv(physeq16S_adj_scaled, file = "physeq16S_adj_scaled_Absolute.csv")
+physeq16S_adj_scaled_count <- round(otu_table(physeq16S_adj_scaled) * scaling_factor_Tetragenococcus_halophilus)
+write.csv(physeq16S_adj_scaled_count, file = "physeq16S_adj_scaled_AbsoluteCount.csv")
 
-# Create new phyloseq object with absolute abundance
-otu <- read.csv("physeq16S_adj_scaled_Absolute.csv", header = TRUE, sep = ",", row.names = 1)
+# Create a new phyloseq object with the calculated absolute counts
+otu <- read.csv("physeq16S_adj_scaled_AbsoluteCount.csv", header = TRUE, sep = ",", row.names = 1)
 otumat <- as.matrix(otu)
 random_tree = rtree(ntaxa(adj_spiked_16S), rooted=TRUE, tip.label=taxa_names(adj_spiked_16S))
 
 adj_scaled_spiked_16S <- phyloseq(otu_table(otumat, taxa_are_rows = TRUE),
-                           tax_table(adj_spiked_16S),
+                           tax_table(physeq16S_adj_scaled),
                            phy_tree(random_tree),
-                           sample_data(adj_spiked_16S))                     
+                           sample_data(physeq16S_adj_scaled))    
+
+#Alternatively you could save time by directly crossing spiked-in calling factor to the ASV table while you are making you new phyloseq object
+
+# Create new phyloseq object with scaled ASV x spiked-in calling factor 
+random_tree <- rtree(ntaxa(spiked_16S), rooted = TRUE, tip.label = taxa_names(spiked_16S))
+physeq16S_adj_scaled <- phyloseq(
+  otu_table(otu_table(spiked_16S) * scaling_factor_Tetragenococcus_halophilus, taxa_are_rows = TRUE),
+  tax_table(spiked_16S),
+  phy_tree(random_tree),
+  sample_data(spiked_16S)
+)
+
+print("You did it, Congratulations!")
+
+                 
