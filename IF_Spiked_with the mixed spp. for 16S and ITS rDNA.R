6d38d7e658c6bc74063edfe50fed39c4267cd440
Mitra <mitra.ghotbi@gmail.com>
2024-03-26 18:38:10 -0500

    Update IF_Spiked_with the mixed spp. for 16S and ITS rDNA.R

--- IF_Spiked_with the mixed spp. for 16S and ITS rDNA.R
+++ IF_Spiked_with the mixed spp. for 16S and ITS rDNA.R
@@ -59,7 +59,7 @@ meta<-read.csv("meta.csv",row.names = NULL,header = T,sep = ",")
 })
 
 # Create phyloseq object with the new metadata consists of spike_volume
-cat("prepareg the new metadata, including spike volumes :D 👍 ~<:<<<<<> 😊 ")
+cat("prepare the new metadata, including spike volumes :D 👍 ~<:<<<<<> 😊 ")
 
 ({
   
